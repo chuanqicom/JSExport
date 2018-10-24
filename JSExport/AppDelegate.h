d3d34ec7d73e49c51a53508b467568b3a6f309fe
塞外神龙 <wangliang_sh@163.com>
2018-10-24 21:01:53 +0800

    Initial Commit

---
+++ JSExport/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSExport
+//
+//  Created by liang wang on 2018/10/24.
+//  Copyright © 2018年 liang wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JSExport/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSExport
+//
+//  Created by liang wang on 2018/10/24.
+//  Copyright © 2018年 liang wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
